4c154bea94bad1001c10182a912b13576ea02222
IamMarcIvanov <45270390+IamMarcIvanov@users.noreply.github.com>
2021-04-21 01:19:09 +0530

    best dqn yet and its plot

---
+++ dqnPerfPlotter.py
@@ -0,0 +1,41 @@
+# %%
+import matplotlib.pyplot as plt
+
+frames = []
+rewards = []
+epsilons = []
+
+# %%
+def find_nth(s, x, n):
+    i = -1
+    for _ in range(n):
+        i = s.find(x, i + len(x))
+        if i == -1:
+            break
+    return i
+
+# %%
+with open('BestYetDQNoutput.txt', 'r') as f:
+    for line in f:
+        if line.startswith('Best'):
+            continue
+        frame_end = line.index(':')
+        frames.append(int(line[: frame_end]))
+        
+        reward_start = find_nth(line, ' ', 6) + 1
+        reward_end = find_nth(line, ',', 2)
+        rewards.append(float(line[reward_start: reward_end]))
+        
+        epsilon_start = find_nth(line, ' ', 8) + 1
+        epsilon_end = find_nth(line, ',', 3)
+        epsilons.append(float(line[epsilon_start: epsilon_end]))
+
+# %%
+plt.plot(frames[:-1], rewards, label='reward')
+plt.legend()
+plt.title('Best Yet DQN Performance: Frames vs Rewards')
+plt.show()
+plt.plot(frames[:-1], epsilons, label='epsilon')
+plt.legend()
+plt.title('Best Yet DQN Performance: Frames vs Epsilon')
+plt.show()
